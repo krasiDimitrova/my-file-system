c9542e3922b94ff7456438da120d2d20be37cb3e
Krasimira Milkova <krasimira_milkova@outlook.com>
2018-09-13 09:40:35 +0300

    Refactor WordCounter tests

--- test/myfilesystem/WordCounterTest.java
+++ test/myfilesystem/WordCounterTest.java
@@ -8,75 +8,86 @@ public class WordCounterTest {
 
     @Test
     public void givenTwoWordsTextWhenCountTextIsInvokedThenReturn2() {
+        WordCounter wc = new WordCounter();
         String text = "test text";
-        assertEquals(2, WordCounter.countText(text));
+        assertEquals(2, wc.countText(text));
     }
 
     @Test
     public void givenTwoWordsTextBeginingWithASpaceWhenCountTextIsInvokedThenReturn2() {
+        WordCounter wc = new WordCounter();
         String text = " test text";
-        assertEquals(2, WordCounter.countText(text));
+        assertEquals(2, wc.countText(text));
     }
 
     @Test
     public void givenOneWordBeginignWithASpaceWhenCountTextIsInvokedThenReturn1() {
+        WordCounter wc = new WordCounter();
         String text = "test ";
-        assertEquals(1, WordCounter.countText(text));
+        assertEquals(1, wc.countText(text));
     }
 
     @Test
     public void givenEmptyTextWhenCountTextIsInvokedThenReturn0() {
+        WordCounter wc = new WordCounter();
         String text = "";
-        assertEquals(0, WordCounter.countText(text));
+        assertEquals(0, wc.countText(text));
     }
 
     @Test
     public void givenTextWithJustSpcaesWhenCountTextIsInvokedThenReturn0() {
+        WordCounter wc = new WordCounter();
         String text = "  ";
-        assertEquals(0, WordCounter.countText(text));
+        assertEquals(0, wc.countText(text));
     }
 
     @Test
     public void givenTextWithPunctoationWhenCountTextIsInvokedThenReturnWordCount() {
+        WordCounter wc = new WordCounter();
         String text = "Test, text! .";
-        assertEquals(3, WordCounter.countText(text));
+        assertEquals(3, wc.countText(text));
     }
 
     @Test
     public void givenTextWithNewLineWhenCountTextIsInvokedThenReturnWordCount() {
+        WordCounter wc = new WordCounter();
         String text = "Test \n text";
-        assertEquals(2, WordCounter.countText(text));
+        assertEquals(2, wc.countText(text));
     }
 
     @Test
     public void givenOneLineFileWhenCountFileIsInvokedThenReturnNumberOfWordsInFile() {
+        WordCounter wc = new WordCounter();
         MyFile test = new MyFile("test");
         test.addLine("test line");
-        assertEquals(2, WordCounter.countFile(test));
+        assertEquals(2, wc.countFile(test));
     }
 
     @Test
     public void givenFileWhenCountFileIsInvokedThenReturnNumberOfWordsInFile() {
+        WordCounter wc = new WordCounter();
         MyFile test = new MyFile("test");
         test.addLine("test line");
         test.addLine("test line");
-        assertEquals(4, WordCounter.countFile(test));
+        assertEquals(4, wc.countFile(test));
     }
 
     @Test
     public void givenEmptyFileWhenCountFileIsInvokedThenReturn0() {
+        WordCounter wc = new WordCounter();
         MyFile test = new MyFile("test");
-        assertEquals(0, WordCounter.countFile(test));
+        assertEquals(0, wc.countFile(test));
     }
 
     @Test
     public void givenFileWithEmptyLineWhenCountFileIsInvokedThenReturnNumberOfWordsInFile() {
+        WordCounter wc = new WordCounter();
         MyFile test = new MyFile("test");
         test.addLine("test line");
         test.addLine("");
         test.addLine("");
         test.addLine("test line");
-        assertEquals(4, WordCounter.countFile(test));
+        assertEquals(4, wc.countFile(test));
     }
 
 }
